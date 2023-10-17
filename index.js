45f1643a4e0f2cc1cae7443e52135afab3ac85d3
Chris Knowles <chris.knowles@thinkmoney.co.uk>
2023-10-17 08:19:04 +0100

    updated index.js ready for first assignment

--- index.js
+++ index.js
@@ -1,87 +1,102 @@
-function Book(title, author, pages, isRead) {
-  this.title = title;
+// All of your book objects are going to be stored in a simple array, so add a function to the script (not the constructor) that can take user’s input and store the new book objects into an array. Your code should look something like this:
+
+const myLibrary = [];
+
+function Book(author, title) {
   this.author = author;
-  this.pages = pages;
-  this.isRead = isRead;
-  this.info = function () {
-    const readStatus = this.isRead ? "read" : "not read yet";
-    return `${title} by ${author}, ${pages} pages,  ${readStatus}`;
-  };
+  this.title = title;
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
-console.log(theHobbit.info());
-
-// Without Prototype Inheritance
-function Dog(name) {
-  this.name = name;
-  this.bark = function () {
-    console.log(`${this.name} barks.`);
-  };
+function addBookToLibrary() {
+  // do stuff here
 }
 
-const dog1 = new Dog("Fido");
-const dog2 = new Dog("Buddy");
+// ✨✨✨✨ Notes ✨✨✨✨
 
-dog1.bark(); // Output: "Fido barks."
-dog2.bark(); // Output: "Buddy barks."
+// function Book(title, author, pages, isRead) {
+//   this.title = title;
+//   this.author = author;
+//   this.pages = pages;
+//   this.isRead = isRead;
+//   this.info = function () {
+//     const readStatus = this.isRead ? "read" : "not read yet";
+//     return `${title} by ${author}, ${pages} pages,  ${readStatus}`;
+//   };
+// }
 
-// Each dog instance has its own copy of the bark method, leading to redundancy in memory usage. If you update the bark method, you would need to do so for each instance separately.
+// const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+// console.log(theHobbit.info());
 
-// With Prototype Inheritance
-function Dog(name) {
-  this.name = name;
-}
+// // Without Prototype Inheritance
+// function Dog(name) {
+//   this.name = name;
+//   this.bark = function () {
+//     console.log(`${this.name} barks.`);
+//   };
+// }
 
-Dog.prototype.bark = function () {
-  console.log(`${this.name} barks.`);
-};
+// const dog1 = new Dog("Fido");
+// const dog2 = new Dog("Buddy");
 
-const dog1 = new Dog("Fido");
-const dog2 = new Dog("Buddy");
+// dog1.bark(); // Output: "Fido barks."
+// dog2.bark(); // Output: "Buddy barks."
 
-dog1.bark(); // Output: "Fido barks."
-dog2.bark(); // Output: "Buddy barks."
+// // Each dog instance has its own copy of the bark method, leading to redundancy in memory usage. If you update the bark method, you would need to do so for each instance separately.
 
-// The bark method is defined in the Dog prototype, and both dog1 and dog2 instances share this method. This reduces memory usage and allows for more efficient updates to the method.
+// // With Prototype Inheritance
+// function Dog(name) {
+//   this.name = name;
+// }
 
-//Another example:
+// Dog.prototype.bark = function () {
+//   console.log(`${this.name} barks.`);
+// };
 
-// Initialize constructor functions
-function Hero(name, level) {
-  this.name = name;
-  this.level = level;
-}
+// const dog1 = new Dog("Fido");
+// const dog2 = new Dog("Buddy");
 
-function Warrior(name, level, weapon) {
-  Hero.call(this, name, level);
+// dog1.bark(); // Output: "Fido barks."
+// dog2.bark(); // Output: "Buddy barks."
 
-  this.weapon = weapon;
-}
+// // The bark method is defined in the Dog prototype, and both dog1 and dog2 instances share this method. This reduces memory usage and allows for more efficient updates to the method.
 
-function Healer(name, level, spell) {
-  Hero.call(this, name, level);
+// //Another example:
 
-  this.spell = spell;
-}
+// // Initialize constructor functions
+// function Hero(name, level) {
+//   this.name = name;
+//   this.level = level;
+// }
 
-// Link prototypes and add prototype methods
-Object.setPrototypeOf(Warrior.prototype, Hero.prototype);
-Object.setPrototypeOf(Healer.prototype, Hero.prototype);
+// function Warrior(name, level, weapon) {
+//   Hero.call(this, name, level);
 
-Hero.prototype.greet = function () {
-  return `${this.name} says hello.`;
-};
+//   this.weapon = weapon;
+// }
 
-Warrior.prototype.attack = function () {
-  return `${this.name} attacks with the ${this.weapon}.`;
-};
+// function Healer(name, level, spell) {
+//   Hero.call(this, name, level);
 
-Healer.prototype.heal = function () {
-  return `${this.name} casts ${this.spell}.`;
-};
+//   this.spell = spell;
+// }
 
-// Initialize individual character instances
-const hero1 = new Warrior("Bjorn", 1, "axe");
-const hero2 = new Healer("Kanin", 1, "cure");
-// With this code we’ve created our Hero constructor with the base properties, created two character constructors called Warrior and Healer from the original constructor, added methods to the prototypes and created individual character instances.
+// // Link prototypes and add prototype methods
+// Object.setPrototypeOf(Warrior.prototype, Hero.prototype);
+// Object.setPrototypeOf(Healer.prototype, Hero.prototype);
+
+// Hero.prototype.greet = function () {
+//   return `${this.name} says hello.`;
+// };
+
+// Warrior.prototype.attack = function () {
+//   return `${this.name} attacks with the ${this.weapon}.`;
+// };
+
+// Healer.prototype.heal = function () {
+//   return `${this.name} casts ${this.spell}.`;
+// };
+
+// // Initialize individual character instances
+// const hero1 = new Warrior("Bjorn", 1, "axe");
+// const hero2 = new Healer("Kanin", 1, "cure");
+// // With this code we’ve created our Hero constructor with the base properties, created two character constructors called Warrior and Healer from the original constructor, added methods to the prototypes and created individual character instances.
